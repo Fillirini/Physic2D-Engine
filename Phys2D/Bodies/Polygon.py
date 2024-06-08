a2933cd3a7fcfa0ff699fc7b801848016cbefe7c
Fillirini <fillirini71@mail.ru>
2024-06-08 21:57:10 +0300

    create Polygon

---
+++ Phys2D/Bodies/Polygon.py
@@ -0,0 +1,12 @@
+from Math.Vector2 import Vector2
+from Phys2D.Bodies.Shape import Shape
+
+
+class Polygon(Shape):
+    vertices: list[Vector2]
+
+    def __init__(self, position: Vector2, vertices: list[Vector2]):
+        super().__init__(position)
+        self.vertices = vertices
+
+    pass
---
+++ Phys2D/Bodies/Shape.py
@@ -0,0 +1,10 @@
+from Math.Vector2 import Vector2
+
+
+class Shape:
+    position: Vector2
+
+    def __init__(self, position: Vector2):
+        self.position = position
+        pass
+    pass

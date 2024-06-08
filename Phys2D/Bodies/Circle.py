01cb099e507e72ea96a7fd2ec8631d6a9633b370
Fillirini <fillirini71@mail.ru>
2024-06-08 21:56:36 +0300

    create Circle

---
+++ Phys2D/Bodies/Circle.py
@@ -0,0 +1,11 @@
+from Math.Vector2 import Vector2
+from Phys2D.Bodies.Shape import Shape
+
+
+class Circle(Shape):
+    radius: float
+
+    def __init__(self, position: Vector2, radius: float):
+        super().__init__(position)
+        self.radius = radius
+    pass

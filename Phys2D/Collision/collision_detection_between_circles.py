8740e836b5d73e3fb66b71c2dbc1fe74b5501cf9
Fillirini <fillirini71@mail.ru>
2024-06-09 11:28:58 +0300

    minor changes

--- Phys2D/Collision/collision_detection_between_circles.py
+++ Phys2D/Collision/collision_detection_between_circles.py
@@ -2,7 +2,7 @@ from Phys2D.Primitives.Circle import Circle
 
 
 def collision_detection_between_circles(circle1: Circle, circle2: Circle) -> bool:
-    distance: float = (circle1.position - circle2.position).get_length()
+    distance: float = Vector2.distance(circle1.position, circle2.position)
     if distance <= circle1.radius + circle2.radius:
         return True
     else:

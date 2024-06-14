6b198de49c19875dfb7a8d3cbd8e2d309b68e52f
Fillirini <fillirini71@mail.ru>
2024-06-14 23:28:17 +0300

    Delete old Physics

--- Phys2D/Collision/collision_detection_between_circle_and_polygon.py
+++
@@ -1,9 +0,0 @@
-from Phys2D.Primitives import *
-
-
-def collision_detection_between_circle_and_polygon(circle: Circle, poly: Polygon) -> bool:
-    for vert in poly.vertices:
-        if Vector2.distance(circle.position, vert) <= circle.radius:
-            return True
-
-    return False
